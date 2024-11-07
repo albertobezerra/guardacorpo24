7d451ccd9c1932ae9aa472069dc5329d71430dd7
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2024-11-07 20:39:16 +0000

    Datas importantes e mapa de risco finalizaos

--- lib/matriz/02_maissaude/datas.dart
+++ lib/matriz/02_maissaude/datas.dart
@@ -1,10 +1,135 @@
 import 'package:flutter/material.dart';
+import 'package:guarda_corpo_2024/admob/banner_ad_widget.dart';
 
 class Datas extends StatelessWidget {
   const Datas({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize:
+            Size.fromHeight(MediaQuery.of(context).size.height * 0.09),
+        child: AppBar(
+          toolbarHeight: 200,
+          title: Text(
+            'Datas importantes'.toUpperCase(),
+            style: const TextStyle(
+              fontFamily: 'Segoe Bold',
+              color: Colors.white,
+              fontSize: 16,
+            ),
+          ),
+          leading: IconButton(
+            icon: const Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          flexibleSpace: const Image(
+            image: AssetImage('assets/images/datas.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: Container(
+                margin: const EdgeInsets.all(30),
+                alignment: AlignmentDirectional.topStart,
+                child: const SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ImportantDatesText(),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const BannerAdWidget(),
+        ],
+      ),
+    );
+  }
+}
+
+class ImportantDatesText extends StatelessWidget {
+  const ImportantDatesText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const List<Map<String, String>> dates = [
+      {
+        "date": "28/02",
+        "description":
+            "Dia Internacional de Prevenção às Lesões por Esforço Repetitivos (LER)"
+      },
+      {"date": "08/03", "description": "Dia Internacional da Mulher"},
+      {"date": "16/04", "description": "Dia Nacional da Voz"},
+      {
+        "date": "28/04",
+        "description":
+            "Dia Mundial em Memória às Vítimas de Acidentes de Trabalho e Dia Mundial de Segurança e Saúde no Trabalho"
+      },
+      {"date": "01/05", "description": "Dia do Trabalhador"},
+      {
+        "date": "02/05",
+        "description": "Dia Nacional de Combate ao Assédio Moral"
+      },
+      {"date": "13/05", "description": "Abolição da Escravatura"},
+      {
+        "date": "18/05",
+        "description":
+            "Dia Nacional ao Abuso e à Exploração Sexual de Crianças e Adolescentes"
+      },
+      {"date": "31/05", "description": "Dia Mundial sem Tabaco"},
+      {
+        "date": "12/06",
+        "description": "Dia Mundial de Combate ao Trabalho Infantil"
+      },
+      {"date": "25/07", "description": "Dia do Trabalhador Rural"},
+      {
+        "date": "27/07",
+        "description": "Dia Nacional de Prevenção de Acidentes de Trabalho"
+      },
+      {
+        "date": "10/11",
+        "description": "Dia Nacional de Prevenção e Combate à Surdez"
+      },
+      {
+        "date": "27/11",
+        "description": "Dia Nacional dos Técnicos de Segurança do Trabalho"
+      },
+      {"date": "09/12", "description": "Dia do Fonoaudiólogo"},
+    ];
+
+    return RichText(
+      textAlign: TextAlign.justify,
+      text: TextSpan(
+        style: const TextStyle(
+          fontSize: 14,
+          color: Colors.black,
+        ),
+        children: dates
+            .map((dateMap) => TextSpan(
+                  children: [
+                    TextSpan(
+                      text: '${dateMap["date"]} - ',
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(text: '${dateMap["description"]}\n\n'),
+                  ],
+                ))
+            .toList(),
+      ),
+    );
   }
 }
--- lib/matriz/02_maissaude/mapa_da_risco.dart
+++ lib/matriz/02_maissaude/mapa_da_risco.dart
@@ -51,15 +51,40 @@ class MapaDaRisco extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     const Text(
-                      'O Mapa de Risco é uma maneira eficiente de proteger seus funcionários, mostrando claramente os riscos que o ambiente de trabalho pode apresentar.\n\nPara conseguir essa visualização, é preciso estudar a empresa de forma efetiva para, assim, chegar a um diagnóstico sobre os perigos de cada de setor.\n\nO Mapa de Risco foi criado na década de 60, pelos italianos, e chegou em terras brasileiras apenas no fim dos anos 70.\n\nCom o aumento da produção industrial e do índice de acidentes, logo em seguida, o método começou a ser utilizado nas fábricas e ambientes de industriais e, em 1992, ele se tornou obrigatório.\n\nDesde então, o Mapa de Risco é exigido em todos os países em que a CIPA (Comissão Interna de Prevenção de Acidentes) está presente e sua ausência pode acarretar em multas de alto de valor.\n\nComo fazer um Mapa de Risco?\n\nCada empresa precisa de um Mapa de Risco adequado para seu segmento, mas alguns itens são comuns a todas, como esses:\n\nREUNIR INFORMAÇÕES SUFICIENTES PARA O ESTABELECIMENTO DE UM DIAGNÓSTICO DA SITUAÇÃO DE SEGURANÇA E SAÚDE NO TRABALHO DO ESTABELECIMENTO.\n\nPOSSIBILITAR A TROCA E DIVULGAÇÃO DE INFORMAÇÕES ENTRE OS TRABALHADORES E ESTIMULAR SUA PARTICIPAÇÃO NAS ATIVIDADES DE PREVENÇÃO.\n\nCONHECER O PROCESSO DE TRABALHO NO LOCAL ANALISADO:\n\nOs trabalhadores: número, sexo, idade, treinamentos profissionais e de segurança e saúde.\nJornada de trabalho.\nOs instrumentos e materiais de trabalho.\nAs atividades exercidas.\nO ambiente.',
-                      textAlign: TextAlign.justify,
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Colors.black,
-                      ),
-                    ),
-                    const Text(
-                      '\nIDENTIFICAR OS RISCOS EXISTENTES NO LOCAL ANALISADO.\n\nIDENTIFICAR AS MEDIDAS PREVENTIVAS EXISTENTES E SUA EFICÁCIA, ENTRE ELAS:\n\nMedidas de proteção coletiva, de organização do trabalho, de proteção individual e de higiene e conforto.\n\n\nDESCOBRIR AS QUEIXAS MAIS COMUNS ENTRE OS FUNCIONÁRIOS EXPOSTOS AOS MESMOS RISCOS, DOENÇAS PROFISSIONAIS JÁ DIAGNOSTICADAS E CAUSAS MAIS FREQUENTES DE AUSÊNCIA NO TRABALHO.\n\nTER CONHECIMENTO DOS LEVANTAMENTOS AMBIENTAIS JÁ REALIZADOS NO LOCAL.\n\nO NÚMERO DE TRABALHADORES EXPOSTOS AO RISCO.\n\nESPECIFICAR OS AGENTES, POR EXEMPLO: QUÍMICOS, ERGONÔMICOS, BIOLÓGICOS OU DE ACIDENTES.\n\nAPÓS APROVAÇÃO DA CIPA, O MAPA DE RISCO DEVE SER EXPOSTO CLARAMENTE EM TODOS OS SETORES ANALISADOS, DE MANEIRA QUE OS FUNCIONÁRIOS POSSAM FACILMENTE VER.\n\n\nPara facilitar a visualização do mapa, os riscos são divididos em cinco grupos, representados por diferentes cores:\n\nGRUPO 1 - RISCOS FÍSICOS (Verde):\n\nVibração, Radiação ionizante e não ionizante, frio, calor, pressões anormais e umidade.\n\nGRUPO 2 - RISCOS QUÍMICOS (Vermelho):\n\nPoeiras, fumos, neblinas, gases, vapores, substancias compostas ou produtos químicos em geral.\n\nGRUPO 3 - RISCOS BIOLÓGICOS (Marron):\n\nVírus, bactérias, fungos, parasitas e bacilos.\n\nGRUPO 4 - RISCOS ERGONÔMICOS (Amarelo):\n\nEsforço físico intenso, levantamento e transporte manual de peso, controle rígido de produtividade, imposição de ritmos excessivos, trabalho em turno noturno, jornadas de trabalho prolongadas, monotonia e repetitividade e outras situações provocadoras de estresses psíquico e físico.\n\nGRUPO 5 - RISCOS DE ACIDENTES (Azul):\n\nArranjo físico inadequado, máquinas e equipamentos sem proteção, iluminação inadequada, probabilidade de incêndios ou explosões, animais peçonhentos, armazenamento inadequado e outras situações que possam acabar em acidentes.\n\nCom os risco qualificados, o próximo passo é fazer a quantificação dos mesmos.\n\nA quantificação dos riscos são feitos com equipamentos específicos para as classes de risco.\n\nNa grande maioria as vezes os mapas de riscos são elaborados com base na plata baixa da empresa, toda via a grande maioria dos colaborades não compreendem.\n\nPara uma melhor compreensão pode ser usada uma foto do local de trabalho.\n\nO Mapa de Risco reduz significantemente as doenças e os acidentes porque conscientiza todos os envolvidos dos perigos apresentados.',
+                      'O Mapa de Risco é uma maneira eficiente de proteger seus funcionários, mostrando claramente os riscos que o ambiente de trabalho pode apresentar.\n\n'
+                      'Para conseguir essa visualização, é preciso estudar a empresa de forma efetiva para, assim, chegar a um diagnóstico sobre os perigos de cada setor.\n\n'
+                      'O Mapa de Risco foi criado na década de 60, pelos italianos, e chegou em terras brasileiras apenas no fim dos anos 70.\n\n'
+                      'Com o aumento da produção industrial e do índice de acidentes, logo em seguida, o método começou a ser utilizado nas fábricas e ambientes industriais e, em 1992, ele se tornou obrigatório.\n\n'
+                      'Desde então, o Mapa de Risco é exigido em todos os países em que a CIPA (Comissão Interna de Prevenção de Acidentes) está presente, e sua ausência pode acarretar em multas de alto valor.\n\n'
+                      '### Como fazer um Mapa de Risco?\n\n'
+                      'Cada empresa precisa de um Mapa de Risco adequado para seu segmento, mas alguns itens são comuns a todas, como:\n\n'
+                      '- Reunir informações suficientes para o estabelecimento de um diagnóstico da situação de segurança e saúde no trabalho do estabelecimento.\n'
+                      '- Possibilitar a troca e divulgação de informações entre os trabalhadores e estimular sua participação nas atividades de prevenção.\n'
+                      '- Conhecer o processo de trabalho no local analisado:\n'
+                      '  - Os trabalhadores: número, sexo, idade, treinamentos profissionais e de segurança e saúde.\n'
+                      '  - Jornada de trabalho.\n'
+                      '  - Os instrumentos e materiais de trabalho.\n'
+                      '  - As atividades exercidas.\n'
+                      '  - O ambiente.\n\n'
+                      '- Identificar os riscos existentes no local analisado.\n\n'
+                      '- Identificar as medidas preventivas existentes e sua eficácia, entre elas:\n'
+                      '  - Medidas de proteção coletiva, de organização do trabalho, de proteção individual e de higiene e conforto.\n\n'
+                      '- Descobrir as queixas mais comuns entre os funcionários expostos aos mesmos riscos, doenças profissionais já diagnosticadas e causas mais frequentes de ausência no trabalho.\n\n'
+                      '- Ter conhecimento dos levantamentos ambientais já realizados no local.\n\n'
+                      '- O número de trabalhadores expostos ao risco.\n\n'
+                      '- Especificar os agentes, por exemplo: químicos, ergonômicos, biológicos ou de acidentes.\n\n'
+                      '- Após a aprovação da CIPA, o Mapa de Risco deve ser exposto claramente em todos os setores analisados, de maneira que os funcionários possam facilmente ver.\n\n'
+                      '### Grupos de Risco\n\n'
+                      'Para facilitar a visualização do mapa, os riscos são divididos em cinco grupos, representados por diferentes cores:\n\n'
+                      '- **Grupo 1 - Riscos Físicos (Verde):** Vibração, radiação ionizante e não ionizante, frio, calor, pressões anormais e umidade.\n'
+                      '- **Grupo 2 - Riscos Químicos (Vermelho):** Poeiras, fumos, neblinas, gases, vapores, substâncias compostas ou produtos químicos em geral.\n'
+                      '- **Grupo 3 - Riscos Biológicos (Marrom):** Vírus, bactérias, fungos, parasitas e bacilos.\n'
+                      '- **Grupo 4 - Riscos Ergonômicos (Amarelo):** Esforço físico intenso, levantamento e transporte manual de peso, controle rígido de produtividade, imposição de ritmos excessivos, trabalho em turno noturno, jornadas de trabalho prolongadas, monotonia e repetitividade e outras situações provocadoras de estresses psíquico e físico.\n'
+                      '- **Grupo 5 - Riscos de Acidentes (Azul):** Arranjo físico inadequado, máquinas e equipamentos sem proteção, iluminação inadequada, probabilidade de incêndios ou explosões, animais peçonhentos, armazenamento inadequado e outras situações que possam acabar em acidentes.\n\n'
+                      '### Quantificação dos Riscos\n\n'
+                      'Com os riscos qualificados, o próximo passo é fazer a quantificação dos mesmos. A quantificação dos riscos é feita com equipamentos específicos para as classes de risco.\n\n'
+                      'Na maioria das vezes, os mapas de riscos são elaborados com base na planta baixa da empresa; no entanto, a maioria dos colaboradores não compreende.\n\n'
+                      'Para uma melhor compreensão, pode ser usada uma foto do local de trabalho. O Mapa de Risco reduz significativamente as doenças e os acidentes porque conscientiza todos os envolvidos dos perigos apresentados.',
                       textAlign: TextAlign.justify,
                       style: TextStyle(
                         fontSize: 14,
