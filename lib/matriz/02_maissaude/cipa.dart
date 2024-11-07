95320b968e490ee5805fa4d7001d3a638a3cb69e
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2024-11-07 18:41:35 +0000

    sinalização de segurança finalizado

--- lib/matriz/02_maissaude/cipa.dart
+++ lib/matriz/02_maissaude/cipa.dart
@@ -13,9 +13,9 @@ class Cipa extends StatelessWidget {
             Size.fromHeight(MediaQuery.of(context).size.height * 0.09),
         child: AppBar(
           toolbarHeight: 200,
-          title: Text(
-            'Cipa'.toUpperCase(),
-            style: const TextStyle(
+          title: const Text(
+            'CIPA',
+            style: TextStyle(
               fontFamily: 'Segoe Bold',
               color: Colors.white,
               fontSize: 16,
@@ -45,26 +45,73 @@ class Cipa extends StatelessWidget {
                 margin: const EdgeInsets.all(30),
                 alignment: AlignmentDirectional.topStart,
                 child: const SingleChildScrollView(
-                    child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                        'A CIPA é uma comissão formada por empregados da empresa para trabalhar em busca de saúde e segurança do trabalho. O foco da comissão é trabalhar para evitar acidentes e doença do trabalho. A Cipa tem representantes dos empregados e do empregador.\n\nO objetivo da CIPA é observar e relatar as condições de risco no ambiente de trabalho e solicitar medidas para reduzir até eliminar os riscos existentes e/ou neutralizar os mesmos.\n\nO mandato da CIPA tem duração de um ano, podendo ser reeleitor por um ano.\n\nO número de membros da CIPA é definido através do dimensionamento previsto na NR 5. Esse dimensionamento leva em consideração a quantidade de empregados e o CNAE da empresa.\n\nO treinamento da CIPA pode ser realizado pelo Técnico em Segurança do Trabalho, Engenheiro de Segurança do Trabalho, ou qualquer ou membro do SESMT, também por entidade ligada ao sindicato da categoria, ou por profissional que possua conhecimento sobre os temas ministrados. Temas esses, que estão listados na NR 5 item 5.32.',
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'A CIPA (Comissão Interna de Prevenção de Acidentes) é composta por colaboradores que visam promover a saúde e a segurança no trabalho, prevenindo acidentes e doenças ocupacionais. A comissão é formada por representantes dos empregados e do empregador.\n\n'
+                        'O principal objetivo da CIPA é observar e relatar condições de risco no ambiente de trabalho, além de solicitar medidas para reduzir, eliminar ou neutralizar esses riscos.\n\n'
+                        'O mandato dos membros da CIPA dura um ano, com possibilidade de reeleição por mais um ano. O número de membros é determinado pelo dimensionamento previsto na NR 5, considerando o número de empregados e o CNAE da empresa.\n\n'
+                        'O treinamento dos membros da CIPA pode ser ministrado por técnicos em segurança do trabalho, engenheiros de segurança, membros do SESMT, profissionais de entidades sindicais, ou outros com conhecimento nos temas listados na NR 5, item 5.32.',
                         textAlign: TextAlign.justify,
                         style: TextStyle(
                           fontSize: 14,
                           color: Colors.black,
-                        )),
-                    Text(
-                      '\n\n\nAtribuições da CIPA\n\nDiscutir e ajudar na investigação dos acidentes ocorridos, na empresa e de trajeto;\n\nSugerir medidas de prevenção e neutralização dos riscos no ambiente de trabalho, que se julguem necessárias;\n\nPromover a divulgação e zelar pela observância das normas de segurança do Ministério do Trabalho, como as normas de segurança da empresa;\n\nPromover o interesse dos empregados pela preservação de acidentes e doenças ocupacionais, ser contagiador das questões de segurança;\n\nRealizar inspeções de segurança na empresa, seja por causa de denúncia dos empregados, do empregador ou iniciativa própria. Relatar os riscos encontrados ao empregador e SESMT para que os mesmos tomem as medidas de correção necessárias;\n\nPromover anualmente em conjunto com o SESMT (onde houver) a Semana Interna de Prevenção de Acidentes – SIPAT;\n\nParticipar anualmente em conjunto com a empresa de campanhas de prevenção a AIDS;\n\nParticipar das reuniões ordinárias (mensais), e extraordinárias (quando houver caso de riscos iminente – risco de morte);\n\nRegistrar as reuniões mensais em livro próprio e entregar e entregar cópias aos membros da CIPA e empregador;\n\nSolicitar cópia das CAT’s emitidas e discuti-las nas reuniões mensais;\n\nSugerir cursos, melhorias e adequações no ambiente de trabalho sempre que necessário;\n\nParticipar com o SESMT (onde Houver) das investigações de acidentes de trabalho, causas e fontes de risco. E acompanhar a implantação das medidas corretivas;\n\nRequisitar ao empregador e analisar informações que tenham interferido na segurança e saúde dos trabalhadores;\n\nRequerer do SESMT (onde houver) e do empregador a paralisação de máquina ou setor que considere haver risco grave e iminente (risco de morte) a saúde e vida do trabalhador;\n\nColaborar na elaboração e implantação dos programas de saúde da empresa, PPRA, PCMSO e outros programas relacionados a saúde no trabalho;\n\nElaborar Mapa de Riscos da empresa em parceria com o SESMT (onde houver), na ocasião entrevistar funcionários sobre riscos encontrados no ambiente de trabalho;\n\n\n\nFunções na CIPA\n\nPresidente: Representante do empregador e indicado por ele;\n\nVice-Presidente: Representante dos empregados, é escolhido dentre os que foram eleitos por voto direto;\n\nSecretário e Vice-Secretário: São escolhidos em comum acordo entre os representantes dos empregados (votados)  e do empregador (indicados);\n\nMembros da CIPA: Representantes dos empregados (votados) e do empregador (indicados).',
-                      textAlign: TextAlign.justify,
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Colors.black,
+                        ),
                       ),
-                    ),
-                  ],
-                )),
+                      SizedBox(height: 20),
+                      Text(
+                        'Atribuições da CIPA',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        '• Auxiliar na investigação de acidentes de trabalho;\n'
+                        '• Sugerir medidas de prevenção e neutralização de riscos;\n'
+                        '• Divulgar e zelar pela observância das normas de segurança;\n'
+                        '• Incentivar a conscientização sobre a segurança no trabalho;\n'
+                        '• Realizar inspeções periódicas e relatar riscos ao SESMT;\n'
+                        '• Promover anualmente a Semana Interna de Prevenção de Acidentes (SIPAT);\n'
+                        '• Participar de campanhas de prevenção à AIDS;\n'
+                        '• Participar das reuniões mensais e extraordinárias;\n'
+                        '• Registrar reuniões e compartilhar as atas com os membros;\n'
+                        '• Discutir CATs emitidas e sugerir melhorias no ambiente de trabalho;\n'
+                        '• Investigar acidentes e acompanhar medidas corretivas;\n'
+                        '• Solicitar paralisação de atividades em caso de risco grave;\n'
+                        '• Colaborar na implementação de programas de saúde no trabalho (PPRA, PCMSO);\n'
+                        '• Elaborar o Mapa de Riscos da empresa junto ao SESMT.',
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        'Funções na CIPA',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        '• Presidente: Representante do empregador, indicado por ele.\n'
+                        '• Vice-Presidente: Representante dos empregados, eleito.\n'
+                        '• Secretário e Vice-Secretário: Escolhidos em comum acordo entre representantes eleitos e indicados.\n'
+                        '• Membros da CIPA: Representantes eleitos pelos empregados e indicados pelo empregador.',
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           ),
--- lib/matriz/02_maissaude/sinalizacao.dart
+++ lib/matriz/02_maissaude/sinalizacao.dart
@@ -1,10 +1,213 @@
 import 'package:flutter/material.dart';
 
+import '../../admob/banner_ad_widget.dart';
+
 class Sinalizacao extends StatelessWidget {
   const Sinalizacao({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize:
+            Size.fromHeight(MediaQuery.of(context).size.height * 0.09),
+        child: AppBar(
+          toolbarHeight: 200,
+          title: Text(
+            'Sinalização de Segurança'.toUpperCase(),
+            style: const TextStyle(
+              fontFamily: 'Segoe Bold',
+              color: Colors.white,
+              fontSize: 16,
+            ),
+          ),
+          leading: IconButton(
+            icon: const Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          flexibleSpace: const Image(
+            image: AssetImage('assets/images/treinamentos.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: Container(
+                margin: const EdgeInsets.all(30),
+                alignment: AlignmentDirectional.topStart,
+                child: const SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'A sinalização de segurança é essencial para orientar e proteger os colaboradores no ambiente de trabalho. Abaixo estão as principais categorias de sinalização e suas especificações:',
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        'Sinalização de Perigo',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        'Esta categoria adverte sobre situações, objetos ou ações que possam causar dano ou lesão pessoal, além de afetar as instalações.',
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        '\nCaracterísticas dos sinais de perigo:',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        '• Forma triangular;\n'
+                        '• Pictograma negro sobre fundo amarelo, com margem negra;\n'
+                        '• A cor amarela deve cobrir pelo menos 50% da superfície da placa.',
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        '\nSinalização de Proibição',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        'Os sinais desta categoria têm como objetivo impedir comportamentos que possam colocar em risco a segurança de indivíduos.',
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        '\nCaracterísticas dos sinais de proibição:',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        '• Forma redonda;\n'
+                        '• Pictograma negro sobre fundo branco, com margem e faixa vermelhas;\n'
+                        '• A faixa diagonal vermelha deve estar a 45º, descendo da esquerda para a direita sobre o pictograma.',
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        'Sinalização de Salvamento ou Emergência',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        'Estes sinais indicam, em caso de perigo, as saídas de emergência, o caminho para o posto de socorro ou o local onde estão disponíveis dispositivos de salvamento.',
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        '\nCaracterísticas dos sinais de salvamento ou emergência:',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        '• Forma retangular ou quadrada;\n'
+                        '• Pictograma branco sobre fundo verde;\n'
+                        '• A cor verde deve cobrir pelo menos 50% da superfície da placa.',
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        'Sinalização de Material de Combate a Incêndio',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        'Os sinais desta categoria indicam a localização de equipamentos de combate a incêndio, para utilização em caso de emergência.',
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        '\nCaracterísticas dos sinais de combate a incêndio:',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        '• Forma retangular ou quadrada;\n'
+                        '• Pictograma branco sobre fundo vermelho;\n'
+                        '• A cor vermelha deve cobrir pelo menos 50% da superfície da placa.',
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const BannerAdWidget(),
+        ],
+      ),
+    );
   }
 }
