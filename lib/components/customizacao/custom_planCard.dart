3882b910545b6274318b3af0c83dfb9dfea0baa1
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2025-04-02 03:40:46 +0100

    Ajuste funciona onde tudo está funcionando em questões de compras e já foi feito ajustes estéticos iniciais

--- lib/components/customizacao/custom_planCard.dart
+++ lib/components/customizacao/custom_planCard.dart
@@ -2,77 +2,84 @@ import 'package:flutter/material.dart';
 
 class CustomPlanCard extends StatelessWidget {
   final String title;
-  final String description;
-  final String price;
+  final String? description; // Opcional
+  final String? price; // Opcional
   final bool isEnabled;
   final String? infoText;
-  final VoidCallback onPressed;
+  final VoidCallback? onPressed; // Opcional
+  final Color backgroundColor;
+  final Color textColor;
+  final Color borderColor;
 
   const CustomPlanCard({
     super.key,
     required this.title,
-    required this.description,
-    required this.price,
+    this.description,
+    this.price,
     this.isEnabled = true,
     this.infoText,
-    required this.onPressed,
+    this.onPressed,
+    this.backgroundColor = Colors.transparent,
+    this.textColor = const Color.fromARGB(255, 0, 104, 55),
+    this.borderColor = const Color.fromARGB(255, 0, 104, 55),
   });
-
-  static const Color borderAndTextColor = Color.fromARGB(255, 0, 104, 55);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: isEnabled ? onPressed : null, // Card inteiro é clicável
+      onTap: isEnabled ? onPressed : null,
       child: Padding(
         padding: const EdgeInsets.only(left: 12, right: 12),
         child: Card(
-          elevation: 0, // Remove sombra para um visual mais limpo
-          color: Colors.transparent, // Sem cor de fundo
+          elevation: 0,
+          color: backgroundColor,
           margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(12),
-            side: const BorderSide(
-              color: borderAndTextColor, // Borda verde escura
+            side: BorderSide(
+              color: borderColor,
               width: 2,
             ),
           ),
           child: Container(
-            width: double.infinity, // Ocupa toda a largura da tela
+            width: double.infinity,
             padding: const EdgeInsets.all(16.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   title,
-                  style: const TextStyle(
-                    color: borderAndTextColor, // Verde escuro
+                  style: TextStyle(
+                    color: textColor,
                     fontSize: 18,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
                 const SizedBox(height: 30),
-                Text(
-                  description,
-                  style: const TextStyle(
-                    color: borderAndTextColor, // Verde escuro
+                if (description != null)
+                  Text(
+                    description!,
+                    style: TextStyle(
+                      color: textColor,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  price,
-                  style: const TextStyle(
-                    color: borderAndTextColor, // Verde escuro
-                    fontSize: 16,
+                if (price != null) ...[
+                  const SizedBox(height: 8),
+                  Text(
+                    price!,
+                    style: TextStyle(
+                      color: textColor,
+                      fontSize: 16,
+                    ),
                   ),
-                ),
+                ],
                 if (infoText != null)
                   Padding(
                     padding: const EdgeInsets.only(top: 8),
                     child: Text(
                       infoText!,
-                      style: const TextStyle(
-                        color: borderAndTextColor, // Verde escuro
+                      style: TextStyle(
+                        color: textColor,
                       ),
                     ),
                   ),
--- lib/matriz/04_premium/paginapremium.dart
+++ lib/matriz/04_premium/paginapremium.dart
@@ -1,9 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:guarda_corpo_2024/components/customizacao/custom_appBar.dart';
 import 'package:guarda_corpo_2024/components/customizacao/custom_planCard.dart';
-import 'package:guarda_corpo_2024/matriz/04_premium/GenericMessagePage.dart';
 import 'package:guarda_corpo_2024/matriz/04_premium/subscription_service.dart';
 import 'package:guarda_corpo_2024/services/provider/userProvider.dart';
 import 'package:in_app_purchase/in_app_purchase.dart';
@@ -80,81 +77,11 @@ class _PremiumPageState extends State<PremiumPage> {
     debugPrint('Iniciando compra para produto: ${product.id}');
     try {
       final purchase = await _subscriptionService.purchaseProduct(product);
-      if (purchase != null && purchase.status == PurchaseStatus.purchased) {
-        if (!context.mounted) return;
-        if (product.id == 'monthly_ad_free') {
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-              builder: (context) => const GenericMessagePage(
-                title: 'Compra Concluída',
-                message: 'Você agora está livre de publicidade!',
-              ),
-            ),
-          );
-        } else if (product.id == 'monthly_full') {
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-              builder: (context) => const GenericMessagePage(
-                title: 'Compra Concluída',
-                message: 'Você agora tem acesso ao conteúdo premium!',
-              ),
-            ),
-          );
-        }
-        await _updateUserSubscription(product.id, purchase);
-      } else {
+      if (purchase == null || purchase.status != PurchaseStatus.purchased) {
         _showSnackBar('Compra não concluída.');
       }
     } catch (e) {
       _showSnackBar('Erro ao processar compra: $e');
-    }
-  }
-
-  Future<void> _updateUserSubscription(
-      String productId, PurchaseDetails purchase) async {
-    final user = FirebaseAuth.instance.currentUser;
-    if (user == null) {
-      _showSnackBar('Usuário não autenticado.');
-      return;
-    }
-
-    DateTime? transactionDate = purchase.transactionDate != null
-        ? DateTime.fromMillisecondsSinceEpoch(
-            int.parse(purchase.transactionDate!))
-        : DateTime.now();
-    DateTime subscriptionExpiresAt =
-        transactionDate.add(const Duration(days: 30));
-
-    if (!mounted) return;
-    final userProvider = Provider.of<UserProvider>(context, listen: false);
-    userProvider.updateSubscription(
-      isLoggedIn: true,
-      isPremium: productId == 'monthly_full',
-      planType: productId,
-      expiryDate: subscriptionExpiresAt,
-      hasEverSubscribedPremium: productId == 'monthly_full'
-          ? true
-          : userProvider.hasEverSubscribedPremium,
-    );
-
-    try {
-      await FirebaseFirestore.instance.collection('users').doc(user.uid).set(
-        {
-          'planType': productId,
-          'expiryDate': Timestamp.fromDate(subscriptionExpiresAt),
-          'subscriptionStatus': 'active',
-          'subscription': FieldValue.delete(),
-          'hasEverSubscribedPremium': productId == 'monthly_full'
-              ? true
-              : FieldValue.serverTimestamp(), // Mantém se já existia
-        },
-        SetOptions(merge: true),
-      );
-      _showSnackBar('Assinatura ativada com sucesso!');
-    } catch (e) {
-      _showSnackBar('Erro ao salvar assinatura: $e');
     }
   }
 
@@ -168,10 +95,8 @@ class _PremiumPageState extends State<PremiumPage> {
     final userProvider = Provider.of<UserProvider>(context, listen: false);
     if (userProvider.planType == 'monthly_full' &&
         userProvider.hasActiveSubscription()) {
-      // Se monthly_full está ativo, desativa todos os outros planos
-      return false;
+      return productId == 'monthly_full';
     }
-    // Caso contrário, permite comprar qualquer plano não ativo
     return !_isPlanActive(productId);
   }
 
@@ -217,25 +142,44 @@ class _PremiumPageState extends State<PremiumPage> {
       ),
       body: Consumer<UserProvider>(
         builder: (context, userProvider, child) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              for (final product in products)
-                CustomPlanCard(
-                  title: product.id == 'monthly_ad_free'
-                      ? 'PLANO BÁSICO'
-                      : 'PLANO FULL',
-                  description: product.id == 'monthly_ad_free'
-                      ? 'Remove a publicidade.'
-                      : 'Desbloqueie todo o conteúdo exclusivo e remova a publicidade.',
-                  price: product.price,
-                  isEnabled: _isPlanEnabled(product.id),
-                  infoText: _isPlanActive(product.id)
-                      ? 'Assinatura ativa até ${_formatDate(userProvider.expiryDate)}'
-                      : null,
-                  onPressed: () => _handlePurchase(context, product),
-                ),
-            ],
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                for (final product in products)
+                  CustomPlanCard(
+                    title: product.id == 'monthly_ad_free'
+                        ? 'PLANO BÁSICO'
+                        : 'PLANO FULL',
+                    description: _isPlanActive(product.id)
+                        ? null
+                        : product.id == 'monthly_ad_free'
+                            ? 'Remove a publicidade.'
+                            : 'Desbloqueie todo o conteúdo exclusivo e remova a publicidade.',
+                    price: _isPlanActive(product.id) ? null : product.price,
+                    isEnabled: _isPlanEnabled(product.id),
+                    infoText: _isPlanActive(product.id)
+                        ? 'Assinatura ativa até ${_formatDate(userProvider.expiryDate)}'
+                        : null,
+                    onPressed:
+                        _isPlanEnabled(product.id) && !_isPlanActive(product.id)
+                            ? () => _handlePurchase(context, product)
+                            : null,
+                    backgroundColor: _isPlanActive(product.id)
+                        ? const Color.fromARGB(255, 0, 104, 55)
+                        : _isPlanEnabled(product.id)
+                            ? Colors.white
+                            : Colors.grey.shade300,
+                    textColor: _isPlanActive(product.id)
+                        ? Colors.white
+                        : const Color.fromARGB(255, 0, 104, 55),
+                    borderColor: _isPlanActive(product.id)
+                        ? Colors.transparent
+                        : const Color.fromARGB(255, 0, 104, 55),
+                  ),
+              ],
+            ),
           );
         },
       ),
